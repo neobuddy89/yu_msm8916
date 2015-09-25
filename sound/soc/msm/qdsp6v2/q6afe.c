7fba0daa434a5a6baad350b7393e03ee2fa124df
Banajit Goswami <bgoswami@codeaurora.org>
2015-09-24 20:19:48 -0700

    ASoC: msm: qdsp6v2: return 0 when wait_for_timeout succeed
    The function wait_event_timeout returns the number of jiffies
    remaining, if the condition it is waiting for, is met before
    timeout happens. Update the return variable to 0, as returning
    a non-zero value in Linux (returned by wait_event_timeout)
    indicates an error.
    
    Signed-off-by: Banajit Goswami <bgoswami@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- sound/soc/msm/qdsp6v2/q6afe.c
+++ sound/soc/msm/qdsp6v2/q6afe.c
@@ -3686,6 +3686,9 @@ int afe_set_digital_codec_core_clock(u16 port_id,
 		pr_err("%s: wait_event timeout\n", __func__);
 		ret = -EINVAL;
 		goto fail_cmd;
+	} else {
+		/* set ret to 0 as no timeout happened */
+		ret = 0;
 	}
 	if (atomic_read(&this_afe.status) != 0) {
 		pr_err("%s: config cmd failed\n", __func__);
